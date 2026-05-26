264a96b7d79fffbb05ab7591dddd1bc86f9a4c73
Javier Sebastian Santacruz Navarro <santacruzjavier218@gmail.com>
2026-05-26 17:42:39 -0500

    feat: creacion del archivo de backend

---
+++ backend/travelfans-b/src/main/java/co/travelfanscolombia/TravelfansApplication.java
@@ -0,0 +1,13 @@
+package co.travelfanscolombia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelfansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelfansApplication.class, args);
+	}
+
+}
---
+++ backend/travelfans-b/src/test/java/co/travelfanscolombia/TravelfansApplicationTests.java
@@ -0,0 +1,13 @@
+package co.travelfanscolombia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelfansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
